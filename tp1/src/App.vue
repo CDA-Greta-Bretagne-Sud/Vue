bb5a2124de4267338dbe1f7f94562ef11c362135
Casinni <pascal@lamy.mobi>
2023-03-03 16:36:28 +0100

    maj correction exercice2

--- tp1/src/App.vue
+++ tp1/src/App.vue
@@ -58,29 +58,7 @@
 <br>
 <!-- exercice2-->
 <exercice-2/>
-<v-table>
-    <thead>
-      <tr>
-        <th></th>
-        <th class="text-left">
-          Country
-        </th>
-        <th class="text-left">
-         Calling Code
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr
-        v-for="(item,index) in pays"
-        :key="item.name"
-      >
-      <td>{{index}}</td>
-        <td>{{ item.name }}</td>
-        <td>{{ item.callingCodes[0] }}</td>
-      </tr>
-    </tbody>
-  </v-table>
+
 
 <br>
 
